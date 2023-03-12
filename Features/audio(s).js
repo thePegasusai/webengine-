c3c77df162edab7d946f2afe477439f626c2cae5
PegasusAi <82119277+thePegasusai@users.noreply.github.com>
2023-03-11 18:27:37 -0800

    Update audio(s).js

--- Features/audio(s).js
+++ Features/audio(s).js
@@ -11,3 +11,7 @@ const hopSize = 256;
 const mfcc = new MeydaExtractor('mfcc', frameSize, hopSize, 13, sampleRate);
 const audioFeatures = mfcc.extract(audioBuffer.getChannelData(0));
 console.log(audioFeatures);
+//we first load an audio file and decode it into an AudioBuffer object using the Web Audio API. We then create an instance of the MeydaExtractor class, which is a JavaScript library for audio feature extraction. We pass the frameSize and hopSize parameters to specify the size of the analysis window and the hop size between successive windows. We also specify the number of MFCC coefficients we want to extract (in this case, 13), and the sample rate of the audio signal.
+
+//Finally, we call the extract method of the mfcc object, passing in the audio data from the first channel of the AudioBuffer object. The method returns an array of MFCC features for each analysis window in the audio signal.
+
